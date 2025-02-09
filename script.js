e91f36210033dfb605bc561c3a2a0b5ab77ad52d
yuneeorhazarika <83413609+yuneeorhazarika@users.noreply.github.com>
2025-02-09 18:39:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Sona'; // Replace with your secret code
+const secretCode = 'moinu'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
